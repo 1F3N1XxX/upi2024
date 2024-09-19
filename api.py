f517fc6ae19c5ae75064f9d90883e00dba09f52d
Vlad Petrov <p3trovvlad1m1r@yandex.ru>
2024-09-19 10:29:53 +0300

    search func

--- api.py
+++ api.py
@@ -1,29 +1,32 @@
 from api import search_vk_posts
 import vk_api as vkpi
-from api_key import key
+from apikey import key
 import numpy as np
 import time
 
-sigmund = 100 # amount of posts to read per use
+sigmund = 100  # amount of posts to read per use
 
 id = 687
 from_id = '-225405295'
 
-keywords = ['space']   #из тгбота
+keywords = ['space']  # из тгбота
 
 print(f'https://vk.com/wall{from_id}_{id}')
+
 
 def search_vk_posts(token, query, count=sigmund):
     vk_session = vkpi.VkApi(token=token)
     vk = vk_session.get_api()
-    posts = vk.newsfeed.search(q = query, count=count)
+    posts = vk.newsfeed.search(q=query, count=count)
     return [{'text': post['text'], 'date': post['date']} for post in posts]
+
 
 def get_vk_posts(token, owner_id, count=sigmund):
     vk_session = vkpi.VkApi(token=token)
     vk = vk_session.get_api()
     posts = vk.wall.get(owner_id=owner_id, count=count)['items']
     return [{'text': post['text'], 'date': post['date']} for post in posts]
+
 
 def filter_posts_by_keywords(posts, keywords):
     filtered_posts = []
--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import asyncio
-import random
-import types
-
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import Command, StateFilter
+import time
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
+
 from config import TOKEN
 from user_states import UserStates
-from keyboard import keyboards
+from keyboards import keyboards
 
 storage = MemoryStorage()
-
 bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=storage)
 
 
 @dp.message(Command("start"))
 async def start(message: types.Message, state: FSMContext):
+    await message.answer("Привет, я бот мониторинга новостей VK для сотрудников ЦОДД.")
+    time.sleep(0.5)
     kb = keyboards[UserStates.BASE]
-    await message.answer("Привет, я бот мониторинга новостей VK для сотрудников ЦОДД", reply_markup=kb)
+    await message.answer("Нажмите кнопку 'Поиск' и введите ключевые слова, что бы подобрать новости.", reply_markup=kb)
+    await state.set_state(UserStates.BASE)
+async def search(message: types.Message, state: FSMContext):
+    if message.text == 'Поиск':
+        markup = types.ReplyKeyboardRemove()
+        kb = keyboards[UserStates.BASE]
+        await message.answer("Теперь введите ключевые слова:",  markup=markup)
+        messages = list()
+        messages.append(message.text)
+        await message.answer(f'{messages}', markup=kb)
     await state.set_state(UserStates.BASE)
 
+# @dp.message(Command("Поиск"))
+# async def search(message: types.Message, state: FSMContext):
+#     await message.answer("Теперь введите ключевые слова:", reply_markup=None)
+#     await state.set_state(UserStates.BASE)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
